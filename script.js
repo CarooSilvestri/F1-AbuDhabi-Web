220a9da7160d6d38799a1c5322990507daffc242
Carolina Silvestri <mcsilvestri19@gmail.com>
2021-10-28 16:01:13 -0300

    jejej better

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-
-
-    window.onscroll = function(){
-        if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-            document.getElementById("barra").style.display = "block";
-        }
+window.onscroll = function(){
+    
+if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    
+    document.getElementById("barra").style.display = "block";
+    document.getElementById("cuerpo").className ="col-11"
     }
+
+    else{
+        document.getElementById("barra").style.display = "none";
+        document.getElementById("cuerpo").className ="col-12"
+    }
+}
+
