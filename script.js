cc3b77cf4a384ae2c86362d05e74be942c0ac7d7
Carolina Silvestri <mcsilvestri19@gmail.com>
2021-10-28 15:41:47 -0300

    on scroll works! i'm so cool

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+    window.onscroll = function(){
+        if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+            document.getElementById("barra").style.display = "block";
+        }
+    }
